952a754f863e6ca05b97882a47049504b1fe4ba8
BimalTimsina <bimaltimsina4@gmail.com>
2024-01-24 14:02:28 +0545

    server setup done

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./config/express.config");
+
+const server = http.createServer(app);
+server.listen(3005, "localhost", (err) => {
+  if (!err) {
+    console.log("Server is running");
+    console.log("Press Ctrl + C to end sever");
+  }
+});
